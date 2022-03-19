469cb6940e03e022f187109d65e1db0d3b0d145f
ithersta <schilyaev.vlad@gmail.com>
2022-03-19 22:18:40 +0300

    Fix field not following naming convention

--- bot/src/main/kotlin/ru/spbstu/eventbot/domain/entities/User.kt
+++ bot/src/main/kotlin/ru/spbstu/eventbot/domain/entities/User.kt
@@ -2,7 +2,7 @@ package ru.spbstu.eventbot.domain.entities
 
 data class User(
     val id: Long,
-    val chat_id: Long,
+    val chatId: Long,
     val email: String,
     val name: String,
     val group: String
