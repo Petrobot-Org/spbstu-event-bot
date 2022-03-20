454cd67f06fb9aee7f0590d1abb6d3c1a30fbf1c
LysenkoAleksandra <orange260302@gmail.com>
2022-03-20 22:13:00 +0300

    Improve SubmitApplicationUseCase because of entity for course

--- bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/SubmitApplicationUseCase.kt
+++ bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/SubmitApplicationUseCase.kt
@@ -2,14 +2,17 @@ package ru.spbstu.eventbot.domain.usecases
 
 import ru.spbstu.eventbot.domain.entities.Application
 import ru.spbstu.eventbot.domain.repository.ApplicationRepository
+import ru.spbstu.eventbot.domain.repository.CourseRepository
 import ru.spbstu.eventbot.domain.repository.UserRepository
 
 class SubmitApplicationUseCase(
     private val applicationRepository: ApplicationRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val courseRepository: CourseRepository
     ) {
-    operator fun invoke(chatId: Long, courseId: Long) {
+    operator fun invoke(chatId: Long, courseName: String) {
         val user = userRepository.findByChatId(chatId)
-        applicationRepository.insert(Application(null, user.id, courseId))
+        val course = courseRepository.findByName(courseName)
+        applicationRepository.insert(Application(null, user.id, course.id))
     }
 }
