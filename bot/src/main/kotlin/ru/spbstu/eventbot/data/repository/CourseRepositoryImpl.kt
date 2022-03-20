b1a49e1f3b48b66135ac4a18f122072572e7e904
LysenkoAleksandra <orange260302@gmail.com>
2022-03-20 22:00:16 +0300

    Add entity Course to bisness logic layer

---
+++ bot/src/main/kotlin/ru/spbstu/eventbot/data/repository/CourseRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package ru.spbstu.eventbot.data.repository
+
+import ru.spbstu.eventbot.data.source.AppDatabase
+import ru.spbstu.eventbot.domain.entities.Course
+import ru.spbstu.eventbot.domain.repository.CourseRepository
+
+class CourseRepositoryImpl(private val database: AppDatabase) : CourseRepository {
+    override fun findByName(name: String): Course {
+        return database.courseQueries.findByName(name, mapper = { id, name ->
+            Course(id, name)
+        }).executeAsOne()
+    }
+}
---
+++ bot/src/main/kotlin/ru/spbstu/eventbot/domain/entities/Course.kt
@@ -0,0 +1,7 @@
+package ru.spbstu.eventbot.domain.entities
+
+data class Course(
+    val id: Long?,
+    val name: String
+    ////val clientId: String
+    )
