556808b94dcec0c13429d16918e9664afb9d34bd
ithersta <schilyaev.vlad@gmail.com>
2022-03-23 19:42:24 +0300

    Implement email validation

--- bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/IsEmailValidUseCase.kt
+++ bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/IsEmailValidUseCase.kt
@@ -1,7 +1,9 @@
 package ru.spbstu.eventbot.domain.usecases
 
+import org.apache.commons.validator.routines.EmailValidator
+
 object IsEmailValidUseCase {
     operator fun invoke(email: String): Boolean {
-        return true // TODO: Implement email validation
+        return EmailValidator.getInstance().isValid(email)
     }
 }
