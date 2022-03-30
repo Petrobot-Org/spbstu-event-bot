bb4a073748aa4da295e66faaa42cada872fd9703
ithersta <schilyaev.vlad@gmail.com>
2022-03-30 23:46:56 +0300

    Implement client name validation

--- bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/IsClientNameValid.kt
+++ bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/IsClientNameValid.kt
@@ -2,6 +2,6 @@ package ru.spbstu.eventbot.domain.usecases
 
 object IsClientNameValid {
     operator fun invoke(name: String): Boolean {
-        return true // TODO: Implement client name validation
+        return name.isNotEmpty()
     }
 }
