25d3cc2c9f88e188db28ad7f32836186198368bd
viktoria.dushechkina <byzaya@icloud.com>
2022-04-04 22:05:33 +0300

    change to matches in IsFullNameValidUseCase.kt

--- bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/IsFullNameValidUseCase.kt
+++ bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/IsFullNameValidUseCase.kt
@@ -3,6 +3,6 @@ package ru.spbstu.eventbot.domain.usecases
 object IsFullNameValidUseCase {
     operator fun invoke(fullName: String): Boolean {
         val regex = Regex(pattern = "([А-ЯЁ][а-яё]+[\\-\\s]?){2,}")
-        return regex.containsMatchIn(input = fullName)
+        return regex.matches(input = fullName)
     }
 }
