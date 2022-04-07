1528e0d4fbd6d994d5e975ad610d380b2af1c798
ithersta <ithersta@users.noreply.github.com>
2022-04-07 19:25:58 +0000

    [MegaLinter] Apply linters fixes

--- bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/GetApplicantsByCourseIdUseCase.kt
+++ bot/src/main/kotlin/ru/spbstu/eventbot/domain/usecases/GetApplicantsByCourseIdUseCase.kt
@@ -3,7 +3,6 @@ package ru.spbstu.eventbot.domain.usecases
 import ru.spbstu.eventbot.domain.entities.Student
 import ru.spbstu.eventbot.domain.permissions.Permissions
 import ru.spbstu.eventbot.domain.repository.ApplicationRepository
-import ru.spbstu.eventbot.domain.repository.ClientRepository
 import ru.spbstu.eventbot.domain.repository.CourseRepository
 
 class GetApplicantsByCourseIdUseCase(
