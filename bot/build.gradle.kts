3c0b225f3623ae1fc01d29268a5880ee1dfeeda2
byzaya <75326533+byzaya@users.noreply.github.com>
2022-04-11 20:17:21 +0300

    del extra implementation

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -19,9 +19,7 @@ dependencies {
     implementation("com.squareup.sqldelight:sqlite-driver:1.5.3")
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.6")
     implementation("commons-validator:commons-validator:1.7")
-    implementation("junit:junit:4.13.2")
     implementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
     testImplementation(kotlin("test"))
 }
 
