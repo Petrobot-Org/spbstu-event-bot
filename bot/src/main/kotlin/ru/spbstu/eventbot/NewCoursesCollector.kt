1d15b4b69ea3ed06f6f64d5559d1a4649e26d7a9
ithersta <ithersta@users.noreply.github.com>
2022-04-22 21:10:04 +0000

    [MegaLinter] Apply linters fixes

--- bot/src/main/kotlin/ru/spbstu/eventbot/NewCoursesCollector.kt
+++ bot/src/main/kotlin/ru/spbstu/eventbot/NewCoursesCollector.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import ru.spbstu.eventbot.domain.repository.StudentRepository
 import ru.spbstu.eventbot.domain.usecases.CreateNewCourseUseCase
 import ru.spbstu.eventbot.domain.usecases.GetMatchingStudentsUseCase
 import ru.spbstu.eventbot.telegram.notifyNewCourse
