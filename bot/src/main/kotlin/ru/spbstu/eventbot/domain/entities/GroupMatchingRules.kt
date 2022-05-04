1967cdc154b5349daa5f8b63c42c2f50333765a3
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2022-05-05 01:06:59 +0300

    GroupMatchingRules was Change toRegex did.

--- bot/src/main/kotlin/ru/spbstu/eventbot/domain/entities/GroupMatchingRules.kt
+++ bot/src/main/kotlin/ru/spbstu/eventbot/domain/entities/GroupMatchingRules.kt
@@ -1,14 +1,53 @@
 package ru.spbstu.eventbot.domain.entities
 
 import java.time.LocalDate
+import kotlin.math.abs
 
 data class GroupMatchingRules(
     val years: Set<Year> = emptySet(),
     val specialities: Set<Speciality> = emptySet()
 ) {
-    // TODO: Реализовать
     fun toRegex(date: LocalDate): Regex {
+        var re: Regex = Regex("0")
+        var year: Int = date.year.mod(10)
+
+        // 2ой семестр закончивается в июле
+        if (date.monthValue > 6) {
+            year++
+        }
+
         // Пустое множество years или specialities означает, что никаких ограничений на них не накладывается
-        return Regex("TODO${hashCode()}")
+        if (specialities.isEmpty() && years.isEmpty()) {
+            re = Regex(pattern = "[взВЗ]?353[0-9]{4}/[0-9]{5}")
+        }
+
+        if (!years.isEmpty() && specialities.isEmpty()) {
+            re = Regex(pattern = "[взВЗ]?353[0-9]{4}/" + abs(year - years.elementAt(0).value) + "[0-9]{4}")
+        }
+        if (!specialities.isEmpty() && years.isEmpty()) {
+            re = Regex(
+                pattern = "[взВЗ]?353" + specialities.elementAt(0).value + "/[0-9]{5}"
+            )
+        }
+        if (!specialities.isEmpty() && !years.isEmpty()) {
+
+            var yearsDate: String = ""
+            for (index in years.indices) {
+                yearsDate += years.elementAt(index).value
+            }
+
+            var specialitiesDate: String = ""
+            val itt = specialities.iterator()
+            while (itt.hasNext()) {
+                specialitiesDate += itt.next().value
+            }
+            println(yearsDate)
+            println(specialitiesDate)
+            re = Regex(
+                pattern = "[взВЗ]?353[" + specialitiesDate + "]/[" + yearsDate + "][0-9]{4}"
+            )
+        }
+
+        return re
     }
 }
